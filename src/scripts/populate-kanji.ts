b3d617d08a719c735b7fc31ff051be8e4a961ca9
Truong Minh Phat <tmphat1312@gmail.com>
2024-08-23 19:33:14 +0700

    feat: add script to populate kanji data

---
+++ src/scripts/populate-kanji.ts
@@ -0,0 +1,34 @@
+import { Kanji, insertKanji } from '@/drizzle/mutations/kanji';
+import { kanjiAdapter } from '@/populating/adapters/kanji-adapter';
+import { scrapeKanji } from '@/populating/scripts/kanji';
+import { scrapeSlugs } from '@/populating/scripts/slugs';
+import { wait } from '@/utils/wait';
+import ora from 'ora';
+
+(async function populate() {
+  const fromLevel = 1;
+  const toLevel = 60;
+
+  for (let level = fromLevel; level <= toLevel; level++) {
+    console.log(`== Level ${level} ==`);
+
+    let spinner = ora();
+    let slugs = await scrapeSlugs(level, 'kanji');
+    let data: Kanji[] = [];
+
+    for (let slug of slugs) {
+      let decodedSlug = decodeURIComponent(slug);
+      spinner.start(`Scraping ${decodedSlug}...`);
+      let kanji = await scrapeKanji(slug);
+      data.push(kanjiAdapter(kanji));
+
+      await wait(225);
+      spinner.succeed(`Scraped ${decodedSlug}`);
+    }
+
+    if (data.length > 0) {
+      await insertKanji(data);
+      data = [];
+    }
+  }
+})();
