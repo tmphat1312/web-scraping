85f6c5408c03ddd583132e71ad59819b17c52bb6
Truong Minh Phat <tmphat1312@gmail.com>
2024-08-24 13:57:58 +0700

    refactor: move populating empty radicals script to radicals scripts

--- src/scripts/populate-radicals.ts
+++ src/scripts/populate-radicals.ts
@@ -1,8 +1,18 @@
-import { Radical, insertRadicals } from '@/drizzle/mutations/radicals';
+import {
+  Radical,
+  insertRadicals,
+  updateRadicalCharacterInSVG,
+} from '@/drizzle/mutations/radicals';
+import { getEmptyRadical } from '@/drizzle/queries/radicals';
 import { radicalAdapter } from '@/populating/adapters/radical-adapter';
-import { scrapeRadical } from '@/populating/scripts/radical';
+import {
+  scrapeRadical,
+  scrapeRadicalCharacterInSVG,
+} from '@/populating/scripts/radical';
 import { scrapeSlugs } from '@/populating/scripts/slugs';
+import { slugify } from '@/utils/slugify';
 import { wait } from '@/utils/wait';
+import { writeFile } from 'fs/promises';
 import ora from 'ora';
 
 (async function populate() {
@@ -28,6 +38,25 @@ import ora from 'ora';
     if (data.length > 0) {
       await insertRadicals(data);
       data = [];
+    }
+  }
+
+  // Populate radicals with empty characters
+  let emptyRadicals = await getEmptyRadical();
+  let spinner = ora();
+
+  for (let radical of emptyRadicals) {
+    spinner.start(`Populating ${radical.name} SVG...`);
+    let slug = slugify(radical.name);
+    let svg = await scrapeRadicalCharacterInSVG(slug);
+
+    if (svg) {
+      await writeFile(`public/radicals/${slug}.svg`, svg);
+      await updateRadicalCharacterInSVG(radical.id, `/radicals/${slug}.svg`);
+      spinner.succeed(`Populated ${radical.name} SVG`);
+      await wait(200);
+    } else {
+      spinner.fail(`Failed to populate ${radical.name} SVG`);
     }
   }
 })();
