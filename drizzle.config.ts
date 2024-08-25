66a80caa987b7eb0ee0454bc5b895c7db2be7530
Truong Minh Phat <tmphat1312@gmail.com>
2024-08-25 11:45:44 +0700

    refactor: move /bin outside of /src

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   dialect: 'sqlite',
   dbCredentials: {
-    url: './src/bin/db.sqlite',
+    url: './bin/db.sqlite',
   },
   schema: './src/drizzle/schemas/*.ts',
   out: './migrations',
