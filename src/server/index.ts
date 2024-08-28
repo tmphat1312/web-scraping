995fed7c73ce901d30a88911258b4fc0084689b3
Truong Minh Phat <tmphat1312@gmail.com>
2024-08-28 10:51:59 +0700

    feat: spin up trpc server using Nextjs route handler

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,29 +1,11 @@
 import { db } from '@/data/db';
 import { radicals } from '@/data/schemas';
-import { createHTTPServer } from '@trpc/server/adapters/standalone';
-import { eq } from 'drizzle-orm';
-import { parser, string } from 'valibot';
 
 import { publicProcedure, router } from './trpc';
 
-const appRouter = router({
+export const appRouter = router({
   radicals: publicProcedure.query(async () => {
     return db.select().from(radicals).limit(10);
   }),
-  radical: publicProcedure.input(parser(string())).query(async ({ input }) => {
-    let result = await db
-      .select()
-      .from(radicals)
-      .where(eq(radicals.name, input));
-    return result.at(0);
-  }),
 });
 export type AppRouter = typeof appRouter;
-
-const server = createHTTPServer({
-  router: appRouter,
-});
-
-server.listen(3000, () => {
-  console.log('Listening on http://localhost:3000');
-});
